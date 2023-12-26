698c5e7ec267f8843379ac919a8d7ef3788566d3
davintjs <davintan99@gmail.com>
2023-12-26 18:19:11 +0800

    Face encoding function

--- FacialAttendance/camera.py
+++ FacialAttendance/camera.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2 as cv
+import face_recognition
 import os
 
-def FaceCapture():
+def RegisterNew():
     videoCapture = cv.VideoCapture(0)
     if not videoCapture.isOpened():
         print("Unable to open camera!")
@@ -15,24 +16,27 @@ def FaceCapture():
         if not ret:
             print("Unable to receive frame!")
             exit()
-            break
         
         # Reaching here we have a valid frame from the camera
         cv.imshow("Display Window", frame) # Display the image captured
 
         if cv.waitKey(1) == ord('t'):
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # Convert the picture to grayscale
+            print("Taking 10 images for encoding...")
+            for i in range(10): # Take 10 images
+                gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # Convert the picture to grayscale
 
-            filePath = 'Images'
-            if not os.path.exists(filePath): # Create the folder if is new user
-                os.makedirs(filePath)
-                imageCounter = 0
-            imageName = "frame_{}.png".format(imageCounter)
+                filePath = 'Images'
+                if not os.path.exists(filePath): # Create the folder if is new user
+                    os.makedirs(filePath)
+                    imageCounter = 0
+                imageName = "frame_{}.png".format(imageCounter)
 
-            cv.imwrite(os.path.join(filePath, imageName), gray)
-            print("{} has been saved.".format(imageName))
+                cv.imwrite(os.path.join(filePath, imageName), gray)
+                print("{} has been saved.".format(imageName))
 
-            imageCounter += 1
+                imageCounter += 1
+
+            print("Done taking images!")
 
         elif cv.waitKey(1) == ord('q'):
             break
@@ -40,3 +44,24 @@ def FaceCapture():
     # Release the video capturing and clean up
     videoCapture.release()
     cv.destroyAllWindows()
+
+    # Encode the images captured
+    EncodeDataset()
+    
+def EncodeDataset():
+    print("Encoding the images captured...")
+
+    datasetPath = 'Images'
+    faceEncodings = []
+
+    for filename in os.listdir(datasetPath):
+        # Load the image
+        imagePath = os.path.join(datasetPath, filename)
+        image = face_recognition.load_image_file(imagePath)
+        imageFaceEncoding = face_recognition.face_encodings(image)[0]
+
+        faceEncodings.append(imageFaceEncoding) # Store this face encoding into our list
+
+    # Reaching here we are done encoding faces in our dataset images
+    np.save('face_encodings.npy', faceEncodings)
+    print("Done encoding images!")
--- FacialAttendance/main.py
+++ FacialAttendance/main.py
@@ -1,7 +1,21 @@
 import camera
 
 def main():
-    camera.FaceCapture()
+    print("Welcome to the facial attendance system\n")
+    print("1. Register new student")
+    print("2. Take attendance")
+    print("3. Exit\n")
+    option = int(input("To get started, please input an option: "))
+    
+    if option == 1:
+        camera.RegisterNew()
+
+    elif option == 2:
+        print("Taking attendance...")
+    elif option == 3:
+        exit()
+    else:
+        print("Invalid option")
 
 if __name__ == "__main__":
     main()
